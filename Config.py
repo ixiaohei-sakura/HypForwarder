80b73096bf4f019ddc5c03e7c70f29739dcb5c8a
ixiaohei <55349702+ixiaohei-sakura@users.noreply.github.com>
2021-07-24 12:35:32 +0800

    Update Config.py

--- Config.py
+++ Config.py
@@ -1,13 +1,13 @@
 class Config:
     class LocalProperties:
-        host = "127.0.0.1"
+        host = "0.0.0.0"
         port = 25565
 
     class TargetProperties:
         # host = "mc.hypixel.net"  # 等效于下面的
         # host = "172.65.230.98"  # 等效与上面的
         host = "127.0.0.1"
-        port = 51817
+        port = 25565
 
     class SocketProperties:
         maxBuffSize = 40960
